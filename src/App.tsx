357e809228d316965134084e4eaac28d88ebe81f
Ziad-Elganzory <ziadmohamed770@outlook.com>
2025-06-16 15:19:23 +0300

    Dynamic Alert Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import Alert from "./components/ui/Alert"
+import { Ban } from 'lucide-react';
 
 const App = () => {
   return (
-    <div>
-      <Alert />
+    <div style={{margin: "2rem"}}>
+      <Alert 
+        type={"alert-danger"} 
+        icon={<Ban size={20}/>}
+        title={"Something went wrong!"}
+        description="An error occurred while processing your request. Please try again later."
+        />
     </div>
   )
 }
--- src/components/ui/Alert/index.tsx
+++ src/components/ui/Alert/index.tsx
@@ -1,19 +1,28 @@
+import type { ReactNode } from 'react';
 import './index.scss';
-import { BellRing,X } from 'lucide-react';
+import {X} from 'lucide-react';
 
 interface IProps {
+  type: string;
+  icon: ReactNode;
+  title:string;
+  description: string;
 }
 
-const Alert = ({}:IProps) => {
+const Alert = ({type = "alert-danger", icon, title, description}:IProps) => {
   return (
-    <div className='alert-wrapper'>
-      <BellRing />
-      <h4>Alert Title!</h4>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Quo incidunt possimus alias aliquid ex velit eveniet recusandae </p>
-      <span>
-        <X />
-      </span>
+    <div className={type}>
+      <div className='alert-header'>
+        <div className="alert-title">
+          <span>
+            {icon}
+          </span>
+          <h4>{title}</h4>
+        </div>
+          <X className='close' size={20}/>
+      </div>
+      <p>{description}</p>
+
     </div>
   )
 }
